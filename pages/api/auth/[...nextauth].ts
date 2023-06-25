03454368a276e59362a45e6c928acde3d685b76b
ritikkatiyar <ritik.katiyar79.hk@gmail.com>
2023-06-25 19:50:01 +0530

    'auth'

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,56 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prismadb from "@/lib/prismadb";
+import { compare } from "bcrypt";
+
+export default NextAuth({
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Please enter your email and password");
+        }
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid email or password");
+        }
+        const isCorrectPasssword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPasssword) {
+          throw new Error("Invalid email or password");
+        }
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt:{
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,17 @@
 import Input from "@/components/input";
+import axios from "axios";
 import Image from "next/image";
 import React, { useCallback, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
   const [variant, setVariant] = useState("login");
@@ -14,6 +21,34 @@ const Auth = () => {
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password]);
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -28,9 +63,9 @@ const Auth = () => {
                 {variant === "register" && (
                   <Input
                     label="Username"
-                    onChange={(e: any) => setUsername(e.target.value)}
+                    onChange={(e: any) => setName(e.target.value)}
                     id="name"
-                    value={username}
+                    value={name}
                   />
                 )}
                 <Input
@@ -49,9 +84,23 @@ const Auth = () => {
                   value={password}
                 />
               </div>
-              <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                {variant === "login" ? "Sign In" : "Register"}
+              <button
+                onClick={variant === "login" ? login : register}
+                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+              >
+                {variant === "login" ? "Login" : "Sign Up"}
               </button>
+
+              <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                <div className="h-10 w-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                  <FcGoogle size={30} />
+                </div>
+
+                <div className="h-10 w-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                  <FaGithub size={30} />
+                </div>
+              </div>
+
               <p className="text-neutral-500 mt-12">
                 {variant === "login"
                   ? "First time using Netflix?"
